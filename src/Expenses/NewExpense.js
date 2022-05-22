b858d79c5c789d0ea4e9433d06b490382793b018
arjun rawat <arjunrawat0722@gmail.com>
2022-05-22 10:20:46 +0530

    my first commit reactwebsite

---
+++ src/Expenses/NewExpense.js
@@ -0,0 +1,36 @@
+// import React from "react";
+// import "./NewExpense.css";
+
+// import ExpenseForm from "./ExpenseForm";
+
+// const NewExpense= ()=> {
+//   return (
+//     <div className="new-expense">
+//       <ExpenseForm></ExpenseForm>
+//     </div>
+//   );
+// }
+// export default NewExpense;
+
+import React from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className='new-expense'>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
